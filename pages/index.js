9a553e2b629844ccec2da6cea79c4f917cb82a7e
Rial <rialparmar007@gmail.com>
2025-02-04 23:54:29 -0800

    page setup

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+//Menu Component home page
+import MenuList from "@/components/MenuList";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-center mt-6">Welcome to Our Food Ordering System</h1>
+      <MenuList />
+    </div>
+  );
+}
