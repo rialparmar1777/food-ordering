12598767d1fea7eddfdef53c48137a7c63f9bf9b
Rial <rialparmar007@gmail.com>
2025-02-13 15:19:37 -0800

    New changes in the folder to solve internal serve error

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-      domains: ['www.themealdb.com'], // Add the external domain for images
-    },
-  };
-  
-  export default nextConfig;
-  
